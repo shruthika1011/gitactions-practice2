a11a9bb132e7a10cce42469781207c85494cefed
Shruthika <shruthikapadma2004@gmail.com>
2025-10-16 20:49:32 +0530

    hgfd

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+export default function App() {
+return (
+<div className="min-h-screen flex items-center justify-center">
+<h1 className="text-2xl font-semibold">React app using git actions</h1>
+</div>
+);
+}
